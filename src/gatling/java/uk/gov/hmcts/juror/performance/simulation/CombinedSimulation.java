b0c1dee8898c78fd9ef7aa304655da4a109a220d
benedwards <ben.edwards@cgi.com>
2024-05-01 12:26:43 +0100

    Added checkin and bulk checkout scenario

--- src/gatling/java/uk/gov/hmcts/juror/performance/simulation/CombinedSimulation.java
+++ src/gatling/java/uk/gov/hmcts/juror/performance/simulation/CombinedSimulation.java
@@ -11,7 +11,7 @@ import static io.gatling.javaapi.core.CoreDsl.rampConcurrentUsers;
 
 public class CombinedSimulation extends BaseSimulation {
 
-    private final static int TOTAL_TEST_TIME_SECONDS = 20; //Minus any ramp up/down time
+    private final static int TOTAL_TEST_TIME_SECONDS = 10; //Minus any ramp up/down time
     private final static int RAMP_TIME_SECONDS = 1;
 
 
@@ -32,21 +32,25 @@ public class CombinedSimulation extends BaseSimulation {
 //                SummonsReplySearchSimulation.getScenarioBuilderStatic(perUnitTime(10, TimeUnit.HOURS))
 //                    .injectClosed(
 //                        simulationProfileClosed(20)
-//                            .toArray(new ClosedInjectionStep[0]))
-                //TODO bulk checking / checkout -- 333 by 20 per hour (17 per hour per user) -- 12 Jurors Per
+//                            .toArray(new ClosedInjectionStep[0])),
+                //Check in & Bulk checkout -- 333 by 20 per hour (17 per hour per user) -- 12 Jurors Per
+//                RecordAttendanceSimulation.getScenarioBuilderStatic(perUnitTime(17, TimeUnit.HOURS))
+//                    .injectClosed(
+//                        simulationProfileClosed(20)
+//                            .toArray(new ClosedInjectionStep[0])),
                 // transaction
                 //your work 180 by 20 per hour (9 per hour per user)
 //                YourWorkSimulation.getScenarioBuilderStatic(perUnitTime(9, TimeUnit.HOURS))
 //                    .injectClosed(
 //                        simulationProfileClosed(20)
-//                            .toArray(new ClosedInjectionStep[0]))
+//                            .toArray(new ClosedInjectionStep[0])),
                 //deferrals - court -- 180 by 20 per hour (9 per hour per user)
 //                DeferralSimulation.getCourtScenarioBuilderStatic(perUnitTime(9, TimeUnit.HOURS))
 //                    .injectClosed(
 //                        simulationProfileClosed(20)
 //                            .toArray(new ClosedInjectionStep[0])),
                 //excusal - court -- 250 by 20 per hour (12.5 per hour per user)
-//                ExcusalSimulation.getCourtScenarioBuilderStatic(perUnitTime(13, TimeUnit.HOUR))
+//                ExcusalSimulation.getCourtScenarioBuilderStatic(perUnitTime(13, TimeUnit.HOURS))
 //                    .injectClosed(
 //                        simulationProfileClosed(20)
 //                            .toArray(new ClosedInjectionStep[0])),
@@ -54,7 +58,7 @@ public class CombinedSimulation extends BaseSimulation {
 //                PostponeSimulation.getScenarioBuilderStatic(perUnitTime(9, TimeUnit.HOURS))
 //                    .injectClosed(
 //                        simulationProfileClosed(2)
-//                            .toArray(new ClosedInjectionStep[0]))
+//                            .toArray(new ClosedInjectionStep[0])),
                 //TODO utilization report (90 courts @ 2 per court per month) (2 per hour per user - 20 users)
                 //TODO expenses tbc
 
---
+++ src/gatling/java/uk/gov/hmcts/juror/performance/simulation/RecordAttendanceSimulation.java
@@ -0,0 +1,48 @@
+package uk.gov.hmcts.juror.performance.simulation;
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import uk.gov.hmcts.juror.performance.Feeders;
+import uk.gov.hmcts.juror.performance.Util;
+import uk.gov.hmcts.juror.performance.gen.FeederGenerator;
+import uk.gov.hmcts.juror.performance.scenario.JurorManagementScenario;
+import uk.gov.hmcts.juror.performance.scenario.LoginScenario;
+import uk.gov.hmcts.juror.performance.scenario.NavBarScenario;
+
+import java.time.Duration;
+import java.util.Objects;
+
+import static io.gatling.javaapi.core.CoreDsl.exec;
+import static io.gatling.javaapi.core.CoreDsl.feed;
+import static io.gatling.javaapi.core.CoreDsl.pace;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+
+public class RecordAttendanceSimulation {
+    public static ScenarioBuilder getScenarioBuilderStatic(int secondsPerTransaction) {
+        Util.resetCounter();
+        return scenario("Record attendance - Check-in / Bulk check out")
+            .group("Record attendance - Check-in / Bulk check out").on(
+                feed(Feeders.OWNER_FEEDER_COURT)
+                    .exec(LoginScenario.login())
+                    .exec(session -> {
+                        String owner = session.getString("owner");
+                        FeederGenerator feederGenerator =
+                            Feeders.getOrCreateByOwnerAndStatus(owner, "2");
+                        return session.set("juror_number_gen", feederGenerator);
+                    })
+                    .forever().on(
+                        pace(Duration.ofSeconds(secondsPerTransaction))
+                            .exec(NavBarScenario.getJurorManagement())
+                            .exec(JurorManagementScenario.getRecordAttendance())
+                            .repeat(12).on(
+                                exec(session -> {
+                                    FeederGenerator gen = Objects.requireNonNull(session.get("juror_number_gen"));
+                                    return session
+                                        .set("juror_number", gen.generate().get("juror_number"));
+                                }).exec(
+                                    JurorManagementScenario.checkInJuror()
+                                )
+                            )
+                            .exec(JurorManagementScenario.checkOutAllJurors())
+                    ));
+    }
+}
