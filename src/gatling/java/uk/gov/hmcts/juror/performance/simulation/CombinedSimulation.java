ba00f3b583b0c7a22c4a6b7554a3dee6999a915f
benedwards <ben.edwards@cgi.com>
2024-05-16 13:43:54 +0100

    Full Load

--- src/gatling/java/uk/gov/hmcts/juror/performance/simulation/CombinedSimulation.java
+++ src/gatling/java/uk/gov/hmcts/juror/performance/simulation/CombinedSimulation.java
@@ -11,8 +11,8 @@ import static io.gatling.javaapi.core.CoreDsl.rampConcurrentUsers;
 
 public class CombinedSimulation extends BaseSimulation {
 
-    private final static int TOTAL_TEST_TIME_SECONDS = 600; //Minus any ramp up/down time
-    private final static int RAMP_TIME_SECONDS = 120;
+    private final static int TOTAL_TEST_TIME_SECONDS = 3600; //Minus any ramp up/down time
+    private final static int RAMP_TIME_SECONDS = 180;
 
     public CombinedSimulation() {
         super();
@@ -23,47 +23,47 @@ public class CombinedSimulation extends BaseSimulation {
         addAssertions(
             setUp(
                 //Juror Record Search  -- 200 by 20 per hour (10 per hour per user)
-//                JurorRecordSearchSimulation.getScenarioBuilderStatic(perUnitTime(10, TimeUnit.HOURS))
-//                    .injectClosed(
-//                        simulationProfileClosed(20, Duration.ofSeconds(0))
-//                            .toArray(new ClosedInjectionStep[0])),
-//                //Summons reply search -- 200 by 20 per hour (10 per hour per user)
-//                SummonsReplySearchSimulation.getScenarioBuilderStatic(perUnitTime(10, TimeUnit.HOURS))
-//                    .injectClosed(
-//                        simulationProfileClosed(20, Duration.ofSeconds(5))
-//                            .toArray(new ClosedInjectionStep[0])),
-//////                //Check in & Bulk checkout -- 333 by 20 per hour (17 per hour per user) -- 12 Jurors Per
+                JurorRecordSearchSimulation.getScenarioBuilderStatic(perUnitTime(10, TimeUnit.HOURS))
+                    .injectClosed(
+                        simulationProfileClosed(20, Duration.ofSeconds(0))
+                            .toArray(new ClosedInjectionStep[0])),
+                //Summons reply search -- 200 by 20 per hour (10 per hour per user)
+                SummonsReplySearchSimulation.getScenarioBuilderStatic(perUnitTime(10, TimeUnit.HOURS))
+                    .injectClosed(
+                        simulationProfileClosed(20, Duration.ofSeconds(5))
+                            .toArray(new ClosedInjectionStep[0])),
+                //Check in & Bulk checkout -- 333 by 20 per hour (17 per hour per user) -- 12 Jurors Per
                 RecordAttendanceSimulation.getScenarioBuilderStatic(perUnitTime(17, TimeUnit.HOURS))
                     .injectClosed(
                         simulationProfileClosed(20, Duration.ofSeconds(10))
+                            .toArray(new ClosedInjectionStep[0])),
+                // transaction
+                //your work 180 by 20 per hour (9 per hour per user)
+                YourWorkSimulation.getScenarioBuilderStatic(perUnitTime(9, TimeUnit.HOURS))
+                    .injectClosed(
+                        simulationProfileClosed(20, Duration.ofSeconds(15))
+                            .toArray(new ClosedInjectionStep[0])),
+                //deferrals - court -- 180 by 20 per hour (9 per hour per user)
+                DeferralSimulation.getCourtScenarioBuilderStatic(perUnitTime(9, TimeUnit.HOURS))
+                    .injectClosed(
+                        simulationProfileClosed(20, Duration.ofSeconds(20))
+                            .toArray(new ClosedInjectionStep[0])),
+                //excusal - court -- 250 by 20 per hour (12.5 per hour per user)
+                ExcusalSimulation.getCourtScenarioBuilderStatic(perUnitTime(13, TimeUnit.HOURS))
+                    .injectClosed(
+                        simulationProfileClosed(20, Duration.ofSeconds(25))
+                            .toArray(new ClosedInjectionStep[0])),
+                //postponements  9 by 2 per hour (9 per hour per user)
+                PostponeSimulation.getScenarioBuilderStatic(perUnitTime(9, TimeUnit.HOURS))
+                    .injectClosed(
+                        simulationProfileClosed(2, Duration.ofSeconds(30))
+                            .toArray(new ClosedInjectionStep[0])),
+                //TODO utilization report (90 courts @ 2 per court per month) (2 per hour per user - 20 users)
+                //Expenses -- 333 by 20 per hour (17 per hour per user) -- 12 Jurors Per
+                ExpenseSimulation.getScenarioBuilderStatic(perUnitTime(17, TimeUnit.HOURS))
+                    .injectClosed(
+                        simulationProfileClosed(20, Duration.ofSeconds(35))
                             .toArray(new ClosedInjectionStep[0]))
-//////                // transaction
-//////                //your work 180 by 20 per hour (9 per hour per user)
-//                YourWorkSimulation.getScenarioBuilderStatic(perUnitTime(9, TimeUnit.HOURS))
-//                    .injectClosed(
-//                        simulationProfileClosed(20, Duration.ofSeconds(15))
-//                            .toArray(new ClosedInjectionStep[0])),
-//////                //deferrals - court -- 180 by 20 per hour (9 per hour per user)
-//                DeferralSimulation.getCourtScenarioBuilderStatic(perUnitTime(9, TimeUnit.HOURS))
-//                    .injectClosed(
-//                        simulationProfileClosed(20, Duration.ofSeconds(20))
-//                            .toArray(new ClosedInjectionStep[0])),
-//////                //excusal - court -- 250 by 20 per hour (12.5 per hour per user)
-//                ExcusalSimulation.getCourtScenarioBuilderStatic(perUnitTime(13, TimeUnit.HOURS))
-//                    .injectClosed(
-//                        simulationProfileClosed(20, Duration.ofSeconds(25))
-//                            .toArray(new ClosedInjectionStep[0])),
-//////          //postponements  9 by 2 per hour (9 per hour per user)
-//                PostponeSimulation.getScenarioBuilderStatic(perUnitTime(9, TimeUnit.HOURS))
-//                    .injectClosed(
-//                        simulationProfileClosed(2, Duration.ofSeconds(30))
-//                            .toArray(new ClosedInjectionStep[0])),
-////                //TODO utilization report (90 courts @ 2 per court per month) (2 per hour per user - 20 users)
-////                //Expenses -- 333 by 20 per hour (17 per hour per user) -- 12 Jurors Per
-//                ExpenseSimulation.getScenarioBuilderStatic(perUnitTime(17, TimeUnit.HOURS))
-//                    .injectClosed(
-//                        simulationProfileClosed(20, Duration.ofSeconds(35))
-//                            .toArray(new ClosedInjectionStep[0]))
 
             ).maxDuration(Duration.ofSeconds(TOTAL_TEST_TIME_SECONDS + (RAMP_TIME_SECONDS * 2)))
         );
@@ -75,7 +75,7 @@ public class CombinedSimulation extends BaseSimulation {
 
 
     private List<ClosedInjectionStep> simulationProfileClosed(int users, Duration delayStart) {
-        delayStart = Duration.ofSeconds(0);//TODO remove
+//        delayStart = Duration.ofSeconds(0);//TODO remove
         return List.of(
             constantConcurrentUsers(0).during(delayStart),
             rampConcurrentUsers(0).to(users)
